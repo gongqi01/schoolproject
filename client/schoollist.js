b867355dc68bf2c7ff8a586c1e3cbf96456753d3
lingongqi <lgqv2007@163.com>
2015-02-26 22:13:23 +0800

    add conditions

--- client/schoollist.js
+++ client/schoollist.js
@@ -8,21 +8,31 @@
   });
 
   Template.schoollist.events({
+    //insert a new school
     'click #submitbutton': function () {
 	var name = $("#nameinput").val();
 	var code = $("#codeinput").val();
 	var addr = $("#addrinput").val();
-	var emdo = "@"+$("#emdoinput").val();
-	InsertSchool(name,code,addr,emdo);
-	$("#addnewschoolform")[0].reset();
+	var emailtxt = $("#emdoinput").val()
+	var emdo = "@"+emailtxt;
+        if(!name || !code || !addr || !emailtxt)
+          alert("Please fill all blanks");
+        else
+	{
+	  InsertSchool(name,code,addr,emdo);
+	  $("#addnewschoolform")[0].reset();
+        }
     },
-
+    
+    //delete an existing school
     'click a[name=deletebutton]':function (event) {
 	var x = event.currentTarget;
 	var id = x.getAttribute("id");
 	Schools.remove({_id:id});
     },
+ 
 
+    //toggle the school details
     'click td[name=details]':function (event) {
 	var x = event.currentTarget;
 	var id = x.getAttribute("id");
@@ -36,6 +46,7 @@
 		Session.set("toggledId", "");
     },
 
+    //edit the school information
     'click a[name=editbutton]':function (event) {
 	var x = event.currentTarget;
 	var id = x.getAttribute("id");
